a1e583533a9fcf8b57f9689a652b6cb6497c275a
GMolter <gmolter8@gmail.com>
2025-09-01 02:22:20 -0400

    Update tiny-store.js

--- src/tiny-store.js
+++ src/tiny-store.js
@@ -1,15 +1,19 @@
-// tiny-store.js
-export const STORE_BUILD = "STORE_BUILD 2025-09-01T06:05Z";
+// /src/tiny-store.js
+export const STORE_BUILD = "STORE_BUILD 2025-09-01T06:20Z";
 
 export function createStore(initial){
   let state = structuredClone(initial);
   const subs = new Set();
   return {
     get: ()=> state,
-    subscribe: (fn)=> { subs.add(fn); return ()=> subs.delete(fn); },
-    replace: (next)=> { state = structuredClone(next); subs.forEach(s=>s(state)); },
-    patch: (partial)=> { Object.assign(state, partial); subs.forEach(s=>s(state)); }
+    subscribe(fn){ subs.add(fn); return ()=> subs.delete(fn); },
+    patch(p){
+      state = Object.assign({}, state, p);
+      subs.forEach(fn=>fn(state));
+    },
+    replace(next){
+      state = structuredClone(next);
+      subs.forEach(fn=>fn(state));
+    }
   };
 }
-
-console.log(STORE_BUILD);
