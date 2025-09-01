03c27a78340d97b46d9f72893e8c5187042d45ea
GMolter <gmolter8@gmail.com>
2025-09-01 02:30:31 -0400

    plz work src/tiny-store

--- src/tiny-store.js
+++ src/tiny-store.js
@@ -1,19 +1,13 @@
-// /src/tiny-store.js
-export const STORE_BUILD = "STORE_BUILD 2025-09-01T06:20Z";
+// STORE_BUILD 2025-09-01T06:05Z
+console.log("STORE_BUILD 2025-09-01T06:05Z");
 
 export function createStore(initial){
   let state = structuredClone(initial);
   const subs = new Set();
   return {
     get: ()=> state,
-    subscribe(fn){ subs.add(fn); return ()=> subs.delete(fn); },
-    patch(p){
-      state = Object.assign({}, state, p);
-      subs.forEach(fn=>fn(state));
-    },
-    replace(next){
-      state = structuredClone(next);
-      subs.forEach(fn=>fn(state));
-    }
+    subscribe: (fn)=> (subs.add(fn), ()=>subs.delete(fn)),
+    patch: (partial)=>{ state = {...state, ...partial}; subs.forEach(fn=>fn(state)); },
+    replace: (next)=>{ state = structuredClone(next); subs.forEach(fn=>fn(state)); }
   };
 }
