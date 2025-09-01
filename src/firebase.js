322bddc90abc5e768a7ae90fe7d6ff77ea023b02
GMolter <gmolter8@gmail.com>
2025-09-01 02:54:15 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,27 @@
-// FIREBASE_BUILD 2025-09-01T06:05Z
-export const build = "FIREBASE_BUILD 2025-09-01T06:05Z"; console.log(build);
-
+// FIREBASE_BUILD 2025-09-01T06:15Z
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import {
-  getDatabase
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-import {
-  getAuth, onAuthStateChanged, signInAnonymously
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAe7u7Ij4CQUrWUDirzEZo0hyEPwjXO_uI",
   authDomain: "olio-cardsagainsthumanity.firebaseapp.com",
-  databaseURL: "https://olio-cardsagainsthumanity-default-rtdb.firebaseio.com",
   projectId: "olio-cardsagainsthumanity",
   storageBucket: "olio-cardsagainsthumanity.firebasestorage.app",
   messagingSenderId: "256442998757",
   appId: "1:256442998757:web:ab26e55db0b5029879990c",
+  databaseURL: "https://olio-cardsagainsthumanity-default-rtdb.firebaseio.com"
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
 export const auth = getAuth(app);
+export const db   = getDatabase(app);
 
-// Anonymous sign-in on boot
-export const authReady = new Promise((resolve,reject)=>{
-  onAuthStateChanged(auth, (u)=>{
-    if (u) return resolve(u);
-    signInAnonymously(auth).catch(reject);
-  }, reject);
+// fast anon sign-in w/ promise
+export const authReady = new Promise((resolve, reject)=>{
+  let settled = false;
+  onAuthStateChanged(auth, (user)=>{
+    if (user && !settled) { settled = true; resolve(user); }
+  });
+  signInAnonymously(auth).catch((e)=>{ if(!settled){ settled = true; reject(e); } });
 });
