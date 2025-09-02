6b00b4ddaf17c4037a5bcfc164cf6ade50d9bdb8
GMolter <gmolter8@gmail.com>
2025-09-01 21:32:36 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,53 @@
-// FIREBASE_BUILD 2025-09-01T06:15Z
+// FIREBASE_BUILD 2025-09-01T07:25Z
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+import {
+  getAuth,
+  signInAnonymously,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  onValue,
+  onDisconnect,
+  remove,
+  update,
+  push,
+  child
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
+/**
+ * Your web app's Firebase configuration (verbatim from you, with databaseURL added).
+ * storageBucket here uses the standard appspot.com domain (works fine with RTDB/Auth).
+ */
 const firebaseConfig = {
   apiKey: "AIzaSyAe7u7Ij4CQUrWUDirzEZo0hyEPwjXO_uI",
   authDomain: "olio-cardsagainsthumanity.firebaseapp.com",
+  databaseURL: "https://olio-cardsagainsthumanity-default-rtdb.firebaseio.com",
   projectId: "olio-cardsagainsthumanity",
-  storageBucket: "olio-cardsagainsthumanity.firebasestorage.app",
+  storageBucket: "olio-cardsagainsthumanity.appspot.com",
   messagingSenderId: "256442998757",
-  appId: "1:256442998757:web:ab26e55db0b5029879990c",
-  databaseURL: "https://olio-cardsagainsthumanity-default-rtdb.firebaseio.com"
+  appId: "1:256442998757:web:ab26e55db0b5029879990c"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db   = getDatabase(app);
+export const db = getDatabase(app);
 
-// fast anon sign-in w/ promise
-export const authReady = new Promise((resolve, reject)=>{
-  let settled = false;
-  onAuthStateChanged(auth, (user)=>{
-    if (user && !settled) { settled = true; resolve(user); }
-  });
-  signInAnonymously(auth).catch((e)=>{ if(!settled){ settled = true; reject(e); } });
+/** Resolves once we have an anonymous user (or existing user). */
+export const authReady = new Promise((resolve, reject) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      resolve(user);
+    } else {
+      signInAnonymously(auth)
+        .then((cred) => resolve(cred.user))
+        .catch(reject);
+    }
+  }, reject);
 });
+
+// Useful re-exports
+export { ref, set, get, onValue, onDisconnect, remove, update, push, child };
